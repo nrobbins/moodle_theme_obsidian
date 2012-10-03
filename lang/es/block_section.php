36f524b413ba3e549bc5a503bc6b2007cb647c78
Nathan Robbins <nirobbins@navitor.com>
2012-10-03 10:35:16 -0400

    Spanish language strings
    My spanish is rusty, corrections are welcome.

---
+++ lang/es/block_section.php
@@ -0,0 +1,33 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'block_section'
+ *
+ * @package   block_section
+ * @copyright 2012 Nathan Robbins
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Section';
+$string['blocksettings'] = 'Configuración';
+$string['blockcourse'] = 'Número de identificación del curso (Dejar este espacio en blanco para usar el curso actual)';
+$string['blockstring'] = 'Sección';
+$string['blocktitle'] = 'Título';
+$string['blockteacheredit'] = 'Los profesores pueden editar este block';
+$string['section:editsections'] = 'Capaz de cambiar la sección mostrada';
+$string['section:editcourses'] = 'Capaz de cambiar el curso de la cual se muestra la sección';
